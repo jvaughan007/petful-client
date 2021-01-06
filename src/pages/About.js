cf9eddd9c116023750a8c9f356958c34fe788ceb
Joshua Vaughan <jvaughan007@yahoo.com>
2021-01-05 18:30:44 -0600

    added description to the abotu page

--- src/pages/About.js
+++ src/pages/About.js
@@ -9,8 +9,11 @@ export default class AboutPage extends Component {
             <h1>
                 Thinkful Adoption Agency
             </h1>
+            <h3>
+                Simply sign-in and be placed in to a queue! When it's your turn, you will be given the ption to adopt a dog or a cat. Thanks for choosing adoption!
+            </h3>
 
-            <br />
+            
 
             <SignIn />
 
