22b2b23fc7609f22a13fe8bab4b620bbdb363dea
Joshua Vaughan <jvaughan007@yahoo.com>
2021-01-05 19:12:30 -0600

    syntax

--- src/pages/About.js
+++ src/pages/About.js
@@ -11,7 +11,7 @@ export default class AboutPage extends Component {
                 Thinkful Adoption Agency
             </h1>
             <h3>
-                Sign in below to join the queue! When you reach the front of the line, you will get a chance to choose a ct or a dog to give a loving new home to!
+                Sign in below to join the queue! When you reach the front of the line, you will get a chance to choose a cat or a dog to give a loving new home to!
             </h3>
 
             <br />
